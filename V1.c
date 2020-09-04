d2218e73cb82b15ff03187637904941012b53c63
zain2703 <zainnawaz2703@gmail.com>
2020-09-05 01:36:02 +0200

    cancatenate

--- V1.c
+++ V1.c
@@ -1,10 +1,27 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h>
 #define MAX_LIMIT 1000 
 int main() 
 { 
 char str[MAX_LIMIT]; 
 fgets(str, MAX_LIMIT, stdin); 
 printf("%s", str); 
-printf("%d \n", (strlen(str)-1));
+printf("%d", (strlen(str)-1));
+char str1[MAX_LIMIT];
+fgets(str1, MAX_LIMIT, stdin);
+strcat(str,str1);
+//puts(str);
+printf("%s", str);
 return 0; 
 } 
+
+/* #include<stdio.h>
+int main() 
+{ 
+    char dest[50] = "This is an"; 
+    char src[50] = " example"; 
+  
+    strcat(dest, src); 
+    printf(dest) ; 
+    return 0; 
+}  */
