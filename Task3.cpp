5796887e91b78658423712afae35e3fb0332aa3a
zain2703 <zainnawaz2703@gmail.com>
2020-09-08 00:03:36 +0200

    max clique geekforgeek task3

--- Task3.cpp
+++ Task3.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int n=5;
-int *arr[n], *d[n], store[n]; 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+const int MAX=5;
+int *arr[MAX],*d[MAX],store[MAX]; 
 bool is_clique(int b) 
 { 
 	// Run a loop for all set of edges 
@@ -17,10 +21,10 @@ bool is_clique(int b)
 
 // Function to find all the sizes 
 // of maximal cliques 
-int maxCliques(int i, int l) 
+int maxCliques(int i, int l, int n) 
 { 
-    cout<<"In max clique"<<endl;
-    cout<<"The values are i="<<i<<" vlaue of l="<<l<<endl;
+//    cout<<"In max clique"<<endl;
+//    cout<<"The values are i="<<i<<" vlaue of l="<<l<<endl;
 	// Maximal clique size 
 	int max_ = 0; 
 
@@ -39,7 +43,7 @@ int maxCliques(int i, int l)
 			max_ = max(max_, l); 
 
 			// Check if another edge can be added 
-			max_ = max(max_, maxCliques(j, l + 1)); 
+			max_ = max(max_, maxCliques(j, l + 1,n)); 
 		} 
 	} 
 	return max_; 
@@ -47,31 +51,32 @@ int maxCliques(int i, int l)
 
 int main() 
 {   
-    int m,k,l,temp,count;
+    int n,m,k,l,temp,count;
     n=m=k=l=temp=count=0; 
-    while(scanf("%d %d",&n, &m)=='\n');
+    //while(scanf("%d %d",&n, &m)=='\n');
+    cin>>n>>m;
     if(n>=1 && n<=800 && m>=0 && m<=10000)
     {
      //   printf("%d %d \n",n,m);
     
-    for (int i=0; i<n; i++) 
+        for (int i=0; i<n; i++) 
          arr[i] = (int *)calloc(n, sizeof(int));  
-    for (int i = 0; i <  m; i++) 
-       {  
-        while(scanf("%d  %d", &k,&l)=='\n');
-            if(k>l)
-            {
-        //      printf("Exit1");
-              return 0;
-            }
-     //       printf("index %d %d \n",k-1,l-1);
-            arr[k-1][l-1] = 1;
-            arr[l-1][k-1] =1;
-            d[arr[k-1][l-1]]++;
-            d[arr[l-1][k-1]]++; 
-       //     printf("%d %d \n",k,l);    
-       } 
-       cout << maxCliques(0, 1);
+            for (int i = 0; i <  m; i++) 
+            {  
+                cin>>k>>l;
+                    if(k>l)
+                    {
+                //      printf("Exit1");
+                    return 0;
+                    }
+            //       printf("index %d %d \n",k-1,l-1);
+                    arr[k-1][l-1] = 1;
+                    arr[l-1][k-1] =1;
+                    d[arr[k-1][l-1]]++;
+                    d[arr[l-1][k-1]]++; 
+            //     printf("%d %d \n",k,l);    
+            } 
+       count=maxCliques(0, 1,n);
   //     printf("Outside input\n");
 /*     for (int i = 0; i <  n; i++) 
      { for (int j = 0; j < n; j++) 
@@ -86,10 +91,11 @@ int main()
        }
        temp=0;   
   //     printf("\n");
-     }
+     /* }
+ */
      if(count==1)
         count++;
-    printf("%d",count);
-    } */
+     cout<<count;
+   }
    return 0; 
 } 
