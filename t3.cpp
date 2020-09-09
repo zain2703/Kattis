bf06ccdb32a8544f6b700c6d40d143d617752731
zain2703 <zainnawaz2703@gmail.com>
2020-09-09 14:33:55 +0200

    Its working and accepted

--- t3.cpp
+++ t3.cpp
@@ -1,72 +1,34 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-void printSet(set<int> s) 
-{ 
-  
-    cout << "Set: "; 
-    for (int x : s) { 
-        cout << x << " "; 
-    } 
-    cout << endl; 
-}
-
-void printVector(vector<int> vec) 
-{ 
-  
-    cout << "Vector: "; 
-    for (int x : vec) { 
-        cout << x << " "; 
-    } 
-    cout << endl; 
-}
-
-int _lis( vector<int>arr, int n, int *max_ref) 
-{ 
-	/* Base case */
-	if (n == 1) 
-		return 1; 
-
-	// 'max_ending_here' is length of LIS 
-	// ending with arr[n-1] 
-	int res, max_ending_here = 1; 
-
-	/* Recursively get all LIS ending with arr[0], 
-	arr[1] ... arr[n-2]. If arr[i-1] is smaller 
-	than arr[n-1], and max ending with arr[n-1] 
-	needs to be updated, then update it */
-	for (int i = 1; i < n; i++) 
-	{ 
-		res = _lis(arr, i, max_ref); 
-		if (arr[i-1] < arr[n-1] && res + 1 > max_ending_here) 
-			max_ending_here = res + 1; 
-	} 
-
-	// Compare max_ending_here with the overall 
-	// max. And update the overall max if needed 
-	if (*max_ref < max_ending_here) 
-	*max_ref = max_ending_here; 
-
-	// Return length of LIS ending with arr[n-1] 
-	return max_ending_here; 
-} 
-
-// The wrapper function for _lis() 
 int lis(vector<int> arr, int n) 
 { 
-	// The max variable holds the result 
-	int max = 1; 
-
-	// The function _lis() stores its result in max 
-	_lis( arr, n, &max ); 
-
-	// returns max 
-	return max; 
+    int *lis, i, j, max = 0; 
+    lis = (int*)malloc(sizeof(int) * n); 
+  
+    /* Initialize LIS values for all indexes */
+    for (i = 0; i < n; i++) 
+        lis[i] = 1; 
+  
+    /* Compute optimized LIS values in bottom up manner */
+    for (i = 1; i < n; i++) 
+        for (j = 0; j < i; j++) 
+            if (arr[i] > arr[j] && lis[i] < lis[j] + 1) 
+                lis[i] = lis[j] + 1; 
+  
+    /* Pick maximum of all LIS values */
+    for (i = 0; i < n; i++) 
+        if (max < lis[i]) 
+            max = lis[i]; 
+  
+    /* Free memory to avoid memory leak */
+    free(lis); 
+  
+    return max; 
 } 
 
 int main() {
-    int n, m,s;
+    int n, m;
     cin >> n >> m;
      
     //generating two vector arrays
