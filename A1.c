2bcd271ae131763639abdad455052ca300ba7ac9
zain2703 <zainnawaz2703@gmail.com>
2020-10-13 15:14:36 +0200

    The first version sucessfully working in git

--- A1.c
+++ A1.c
@@ -3,20 +3,27 @@
 
 void clip (char *str)
 { 
-   int i=0 ;
+   int mod,i=0;
+   int inc=0;
 //   printf("Printing in main =%c \n",str[0]);
    while(str[i]!=0){
+       mod=i%3;
+       if(mod==0 && str[i]!='P')
+                inc++;   
+       else if(mod==1 && str[i]!='E')
+                inc++;
+       else if (mod==2 && str[i]!='R')
+                inc++;
 //     printf("Printing inside the while =%c \n",str[i]);
      i++;  
 //     printf("Size of array =%d \n",size);
   }
- // printf("Printing outside the while =%c \n",str[i]);
+  printf("%d \n",inc);
 }
 
 int main()
 {
     char *str=(char*)calloc(300, sizeof(char)); ;
     scanf("%s",str);
-    printf("Printing in main =%s \n",str);
     clip(str);
 }
