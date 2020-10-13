11133535193e303a79a97f1de483b689a423f050
zain2703 <zainnawaz2703@gmail.com>
2020-10-13 14:56:16 +0200

    Using the while to find the number of allocated space

--- A1.c
+++ A1.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 #include<string.h>
 
-void decode (char *str)
-{
-   printf("Printing in main =%s",str);
-
+void clip (char *str)
+{ 
+   int i=0 ;
+//   printf("Printing in main =%c \n",str[0]);
+   while(str[i]!=0){
+//     printf("Printing inside the while =%c \n",str[i]);
+     i++;  
+//     printf("Size of array =%d \n",size);
+  }
+ // printf("Printing outside the while =%c \n",str[i]);
 }
 
 int main()
 {
-    char *str=malloc(sizeof(char) * 300);
+    char *str=(char*)calloc(300, sizeof(char)); ;
     scanf("%s",str);
-    printf("Printing in main =%s",str);
-    decode(str);
+    printf("Printing in main =%s \n",str);
+    clip(str);
 }
