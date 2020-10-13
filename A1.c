90020a47f2a52287835adbabfb45c4a01534d968
zain2703 <zainnawaz2703@gmail.com>
2020-10-13 14:36:20 +0200

    printing string in function

--- A1.c
+++ A1.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 
+void decode (char *str)
+{
+   printf("Printing in main =%s",str);
+
+}
+
 int main()
 {
-printf("Inside the main");
+    char *str=malloc(sizeof(char) * 300);
+    scanf("%s",str);
+    printf("Printing in main =%s",str);
+    decode(str);
 }
