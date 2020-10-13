a100bf7d032ff5670659f154590eaf63302f81de
zain2703 <zainnawaz2703@gmail.com>
2020-10-13 15:33:55 +0200

    Final version with comments uploaded for easy task

--- A1.c
+++ A1.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 void clip (char *str)
 { 
    int mod,i=0;
-   int inc=0;
-//   printf("Printing in main =%c \n",str[0]);
+   int day=0;
+   // while condition to find the numer of elements in array.
    while(str[i]!=0){
+       // The clipper size at most is 3
        mod=i%3;
+      // If condiiton to not change if 'PER' already exist.
        if(mod==0 && str[i]!='P')
-                inc++;   
+                day++;   
        else if(mod==1 && str[i]!='E')
-                inc++;
+                day++;
        else if (mod==2 && str[i]!='R')
-                inc++;
-//     printf("Printing inside the while =%c \n",str[i]);
+                day++;
      i++;  
-//     printf("Size of array =%d \n",size);
   }
-  printf("%d \n",inc);
+  printf("%d \n",day);
 }
 
 int main()
-{
-    char *str=(char*)calloc(300, sizeof(char)); ;
+{   char *str=(char*)calloc(300, sizeof(char)); ;
     scanf("%s",str);
+    //print the clip
     clip(str);
+    free (str);
 }
