b22a533908fb14267bccae33233b368748fd08fb
zain2703 <zainnawaz2703@gmail.com>
2020-10-19 00:50:12 +0200

    this python file is working getting issue display

--- A2.py
+++ A2.py
@@ -1,17 +1,39 @@
 import sys
 from collections import deque 
 
+def sum(op,operand1,operand2):  
+    print(op,operand1,operand2)
+    if(op=='+'):
+        s=int(operand1)+int(operand2)
+        return (s)
+    elif(op=='-'):
+        s=int(operand1)-int(operand2)
+        print(s)
+        return (s)
+    elif(op=='*'):
+        s=int(operand1)*int(operand2)
+        return (s)
 
-operations = { '+':lambda a,b:a+b,
-               '-':lambda a,b:a*b,
-               '*':lambda a,b:a*b}
+def printvector(arr):
+        while(len(arr)>0):
+#           print(arr.pop(),end= " ")
+           print(arr.pop())
 
 #for a,arr in enumerate(sys.stdin):
-arr =input()
-arr=arr.split()
-arr.reverse()
-for index,x in enumerate(arr):
-    print(index,x)
-#    if(x=='*'or x=='+' or x=='-'): 
+i=True
+iterator=0
+while(i):
+    arr =input()
+    arr=arr.split()
+    arr.reverse()
+    for index,x in enumerate(arr):
+        print(index,x)
+        if(x=='*'or x=='+' or x=='-'): 
+            if(arr[index-1].isnumeric() and arr[index-2].isnumeric()):
+                new=sum(x,arr[index-1],arr[index-2])
+                arr[index]= new
+                arr.pop(index-1) 
+                arr.pop(index-2)
+    iterator=iterator+1
+    print(f'Case {iterator}: {arr.pop()}')
 
-    
