53060d049b2c035932674ccb3a9a047dd85d7964
zain2703 <zainnawaz2703@gmail.com>
2020-10-19 01:37:43 +0200

    compiler error due to import and #

--- A2.py
+++ A2.py
@@ -1,14 +1,14 @@
-import sys
-from collections import deque 
+#import sys
+#from collections import deque 
 
 def sum(op,operand1,operand2):  
-    print(op,operand1,operand2)
+#    print(op,operand1,operand2)
     if(op=='+'):
         s=int(operand1)+int(operand2)
         return (s)
     elif(op=='-'):
         s=int(operand1)-int(operand2)
-        print(s)
+#        print(s)
         return (s)
     elif(op=='*'):
         s=int(operand1)*int(operand2)
@@ -16,18 +16,18 @@ def sum(op,operand1,operand2):
 
 def printvector(arr):
         while(len(arr)>0):
-#           print(arr.pop(),end= " ")
-           print(arr.pop())
+          print(arr.pop(),end= " ")
+#           print(arr.pop())
 
 #for a,arr in enumerate(sys.stdin):
 i=True
 iterator=0
 while(i):
-    arr =input()
+    arr =str(input())
     arr=arr.split()
     arr.reverse()
     for index,x in enumerate(arr):
-        print(index,x)
+#        print(index,x)
         if(x=='*'or x=='+' or x=='-'): 
             if(arr[index-1].isnumeric() and arr[index-2].isnumeric()):
                 new=sum(x,arr[index-1],arr[index-2])
@@ -35,5 +35,9 @@ while(i):
                 arr.pop(index-1) 
                 arr.pop(index-2)
     iterator=iterator+1
-    print(f'Case {iterator}: {arr.pop()}')
+    print('\n')
+    print(f'Case {iterator}:')
+    printvector(arr)
+    print('\n')
+ #   arr.clear()
 
