201aed59aba3111920ab00421f5ca932cf20a8fd
zain2703 <zainnawaz2703@gmail.com>
2020-10-20 20:29:00 +0200

    using recursion to solve it

--- A2.py
+++ A2.py
@@ -1,29 +1,13 @@
 import sys
 from collections import deque 
 
-#def sum(op,operand1,operand2):  
-#    print(op,operand1,operand2)
-#    if(op=='+'):
-#        s=int(operand1)+int(operand2)
-#        return (s)
-#    elif(op=='-'):
-#        s=int(operand1)-int(operand2)
-#        print(s)
-#        return (s)
-#    elif(op=='*'):
-#        s=int(operand1)*int(operand2)
-#        return (s)
-
-#def printvector(arr):
-#        while(len(arr)>0):
-#          print(arr.pop(),end= " ")
-#           print(arr.pop())
-
-fun = {
-     '+': lambda x, y: x+y,
-     '-': lambda x, y: x-y,
-     '*': lambda x, y: x*y,
-}
+def recursion(arr,index):
+    if(arr[0] != '+' or arr[0] != '-' or arr[0] != '*'):
+        return arr[index],index+1
+    operator= arr[index]
+    operand1,operand1_index = recursion() 
+    operand1,operand1_index = recursion()
+    return 0,0
 
 #for a,arr in enumerate(sys.stdin):
 i=True
@@ -33,46 +17,5 @@ while(i):
     arr = sys.stdin.readline()
 #    print(arr)
     arr=arr.split()
-    arr.reverse()
-    output= []
-    operator=[]
-    for index,x in enumerate(arr):
-#        print(index,x)
-#        output=str(" ") + str(x) + str(output)
-#        output=" ".join((x,output))
-        if(x=='*'or x=='+' or x=='-'):
-            operator.append(x)
-        else:
-            output.append(x)
-        if len(operator)>0 and len(output) >1: 
-            operand1=output.pop()
-            operand2=output.pop()
-#            operand1.isdecimal()
-            #print(operand1,operand2)
-            if(operand1.isdecimal() and operand2.isdecimal()):
-#                print(index,x,arr[index-1],arr[index-2])
-#                print(output)
-                 new=str(fun[str(operator.pop())](int(operand1),int(operand2)))
-                 output.append(new)
-#                arr[index]= new
-#                arr.pop(index-1) 
-#                output[1:]
-#                arr.pop(index-2) 
-#                output[1:]
-            ##    print(output)
-            else:
-                new1 = operator.pop()+" "+operand1+ " "+operand2
-                output.append(new1)
-   #     print(output)
-   # print(output[0]) 
-    
- #   print('\nCase {}:'.format(case))
-   # print(*output, sep='')
-#    if(len(output)==0):
-#        print(input) """
-    if not len(output):
-        print('\nCase {}: {}'.format(case,' '))   
-    else:        
-        print('\nCase {}: {}'.format(case,output.pop()))
-    case=case+1
-    arr.clear()
+    print(arr)    
+    output,index=recursion(arr,0)
--- Polish.cpp
+++ Polish.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 #include<sstream>
 #include<string>
-#include<ctype.h>
-#include<iostream>
+//#include<ctype.h>
+//#include<iostream>
 
 // For debuging
 using namespace std;
@@ -64,14 +64,22 @@ string evaluate(string operators, string operand1, string operand2)
     return to_string(result);
 }
 
+string polish(string exp, int index)
+{ 
+  if(exp[index]!='+' || exp[index] !='-' || exp[index] !='*')
+       cout<<" "<<exp[index]<< " " << index+1;
+//        polish(exp[index],index++);
+
+}
+
 int main() { 
     string ch,pr,sub,sum; bool loop=true;
     vector<string> input;
     vector<string> operands;
     vector<string> operators; 
-    int iterator=0;
+    int index,iterator=0;
     while(loop)
-    { iterator++;
+    {   
       getline(cin,ch);
       istringstream iss(ch);
       iss >> sub;
@@ -80,51 +88,6 @@ int main() {
         input.push_back(sub);
         iss >> sub;
       }
-      sub=" ";
-//      cout<<sub;
-/*       cout<<"Input ";
-      printVector(input); */
-      for (auto ir=input.rbegin(); ir !=input.rend(); ++ir)
-      {  
-        if((*ir)=="+" || (*ir)=="-" || (*ir)=="*")
-        {
-          operators.push_back(*ir);
-        }
-        else
-        { 
-          operands.push_back(*ir);
-              //flag=filter(*ir,pr);
-              //cout<<"the flag"<<flag<<endl;
-        }
-/*           cout<<"value of operands";
-          printVector(operands);
-          cout<<"value of operators";
-          printVector(operators);
-         cout<<"size of operators"<<operators.size()<<endl; */
-        if(operators.size()>0 && operands.size()>1)
-        {   
-            //pr=operands.at(operands.size()-2);
-            pr=operands.at(operands.size()-2);
-         //   cout<<"The operand 1="<<operands.back()<<"The operand 2="<<pr<<endl;
-            
-            if(filter(operands.back(),pr))
-            {
-                  sum=evaluate(operators.back(),operands.back(),pr);
-                  operands.pop_back();
-                  operands.pop_back();
-                  operands.push_back(sum);
-                  operators.pop_back();
-            }
-            else
-            { string tmp = operators.back(); operators.pop_back();
-                 tmp= to_string(operators.pop_back()) +" "+ tmp ; 
-            //  cout<<ch;
-              operands.push_back(operators.front());
-              operators.pop_back(); 
-            }           
-        }
-      } //* - 6 + x -6 - 3 * 0 c
-     cout<<"case "<<iterator<<": ";ReverseVector(operands); cout<<endl;
-     operands.clear();operators.clear();input.clear();
-    }  
+        ch=polish(ch,index);
+    }
 }
