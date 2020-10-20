075aceed35ae00ed79fd1bc5295dbe6b91978c81
zain2703 <zainnawaz2703@gmail.com>
2020-10-21 00:02:29 +0200

    final version uploaded

--- A2.py
+++ A2.py
@@ -1,40 +1,27 @@
 import sys
-from collections import deque 
+def is_numeric(num):
+    if len(num) > 1:
+        return num.lstrip("-").isnumeric()
+    return num.isnumeric()
 
 def sum(op,operand1,operand2):  	
-#    print(op,operand1,operand2)	#    print(op,operand1,operand2)
-    if(op=='+'):	#    if(op=='+'):
-        return str(int(operand1)-int(operand2))	#        s=int(operand1)+int(operand2)
-        return (s)	#        return (s)
-    elif(op=='-'):	#    elif(op=='-'):
-        return str(int(operand1)-int(operand2))	#        s=int(operand1)-int(operand2)
-#        print(s)	#        print(s)
-        return (s)	#        return (s)
-    elif(op=='*'):	
+    if(op=='+'):	
+        return str(int(operand1)+int(operand2))	
+    elif(op=='-'):	
         return str(int(operand1)-int(operand2))	
-
-fun = {'+': lambda x,y: x+y, '-': lambda x,y: x-y,'*': lambda x,y: x*y,}
+    elif(op=='*'):	
+        return str(int(operand1)*int(operand2))	
 
 def recursion(arr,index):
-    if(arr[index] not in fun):
-        print (arr[index])
+    if(arr[index] != '+' and arr[index] != '-' and arr[index] != '*'):
         return arr[index],index+1
     operator= arr[index]
     operand1,next_index = recursion(arr,index+1) 
     operand2,next_index = recursion(arr,next_index)
-    print("The value of =", operator, "The value of op1=", operand1, "The value of op2=", operand2)
-    if(operand1.isnumeric() and operand2.isnumeric()):
-        print("The value of =", operator, "The value of op1=", operand1, "The value of op2=", operand2)
-        return str(fun[operator](int(operand1),int(operand2))), next_index 
+    if(is_numeric(operand1) and is_numeric(operand2)):
+        return sum(operator,operand1,operand2), next_index 
     return ' '.join((operator, operand1, operand2)), next_index
 
-#for a,arr in enumerate(sys.stdin):
-i=True
-case=1
-#print(fun['+'](2,3))
-for line in sys.stdin.readlines():
-#    print(arr)
-    line=line.split()
-    print(line)    
-    output,index=recursion(line,0)
-    print('Case {}:{}'.format(case,output))
+for case, line in enumerate(sys.stdin):   
+    output,index=recursion(line.split(),0)
+    print('Case {}: {}'.format(case+1,output))
