4109e859e2536238a1abe9326680184fb36c8229
zain2703 <zainnawaz2703@gmail.com>
2020-11-09 23:56:23 +0100

    taking the inputs in order

--- TourDeFrance.cpp
+++ TourDeFrance.cpp
@@ -4,7 +4,34 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 
+void printVector(vector<float> vec) 
+{ 
+  
+    cout << "Vector: "; 
+    for (int x : vec) { 
+        cout << x << " "; 
+    } 
+    cout << endl; 
+}
+
 int main() 
-{   
-  cout<<"The OUTPUT FOR THE TOUR_DE_France";
+{  int i, j;
+   float spikes;
+   cin>>i>>j;
+   vector<float> front;
+   vector<float> rear;
+   for (int ii=0;ii<j;ii++)
+   {   
+       if(ii<i){
+        cin>>spikes;
+        front.push_back(spikes);
+       }
+       cin>>spikes;
+       rear.push_back(spikes);
+   }
+   cout <<"The front " ;
+   printVector(front);
+   cout <<"The rear " ;
+   printVector(rear);
+
 } 
