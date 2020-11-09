d3955ce65d5a7dce3569541eda295733d7cc6506
zain2703 <zainnawaz2703@gmail.com>
2020-11-10 00:33:44 +0100

    The while conditon is working

--- TourDeFrance.cpp
+++ TourDeFrance.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 void printVector(vector<float> vec) 
 { 
-  
     cout << "Vector: "; 
     for (int x : vec) { 
         cout << x << " "; 
@@ -15,23 +14,28 @@ void printVector(vector<float> vec)
 }
 
 int main() 
-{  int i, j;
-   float spikes;
-   cin>>i>>j;
+{  int j, i;
+   float spikes=1;
+   int cond=1;
    vector<float> front;
    vector<float> rear;
-   for (int ii=0;ii<j;ii++)
-   {   
-       if(ii<i){
+   cin>>i;
+   while(spikes!=0)
+   { cin>>j;
+    for (int ii=0;ii<j;ii++)
+    {   
+        if(ii<i){
+            cin>>spikes;
+            front.push_back(spikes);
+        }
         cin>>spikes;
-        front.push_back(spikes);
-       }
-       cin>>spikes;
-       rear.push_back(spikes);
+        rear.push_back(spikes);
+    }
+    cin>>spikes;
+    i=spikes;
    }
    cout <<"The front " ;
    printVector(front);
    cout <<"The rear " ;
    printVector(rear);
-
 } 
