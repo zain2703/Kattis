699162b2ba0ae55bde27199ee7f517780ae2f861
zain2703 <zainnawaz2703@gmail.com>
2020-11-09 22:30:05 +0100

    Solving the tour_de_france problem

---
+++ TourDeFrance.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{   
+  cout<<"The OUTPUT FOR THE TOUR_DE_France";
+} 
